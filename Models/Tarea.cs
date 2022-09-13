324719138262aada62a886b5bbf8390ce6fad6a9
Nicolás Barceló <ferreret@gmail.com>
2022-09-13 14:17:17 +0200

    Creados los data annotations en los modelos

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
+
         public string Descripcion { get; set; }
+
         public Prioridad PrioridadTarea { get; set; }
+
         public DateTime FechaCreacion { get; set; }
+
         public virtual Categoria Categoria { get; set; }
+
+        [NotMapped] // No aparecerá en la base de datos
+        public string Resumen { get; set; }
     }
 
     public enum Prioridad
